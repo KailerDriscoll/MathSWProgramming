baef0c3c7e599de3af55e953307cfd86bb5e2ba2
KailerDriscoll <94393070+KailerDriscoll@users.noreply.github.com>
2022-06-25 10:36:23 -0600

    Delete Kailer_Driscoll_Week4Class directory

--- Kailer_Driscoll_Week4Class/Ry.m
+++
@@ -1,8 +0,0 @@
-function R = Ry(a1)
-%RY Summary of this function goes here
-%   Detailed explanation goes here
-R = [ cos(a1)  0 sin(a1)
-            0  1       0
-      -sin(a1) 0 cos(a1)];
-end
-
--- Kailer_Driscoll_Week4Class/Rz.m
+++
@@ -1,8 +0,0 @@
-function R = Rz(a1)
-%RY Summary of this function goes here
-%   Detailed explanation goes here
-R = [ cos(a1)  -sin(a1) 0
-      sin(a1)   cos(a1) 0
-            0         0 1];
-end
-
